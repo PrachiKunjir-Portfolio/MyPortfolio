34c8286a3a8ce4be4b711f525019596217e12f5b
Prachi Kunjir <prachikunjir11@gmail.com>
2023-06-07 12:45:38 -0400

    upadtes

--- src/components/Work/workPage.js
+++ src/components/Work/workPage.js
@@ -112,7 +112,26 @@ const WorkPage = () => {
                   Front-end Developer</h1>
                   <h1 className="text-1xl">May 2022 - Dec 2022</h1>
                 <p className="mt-5 opacity-70">
-                Improved user experience of Commvault’s internal design site through 30+ user interviews and research. Built 53 web pages using react components reducing code by 30%. Developed REST API leveraging Node.js and Express to persist the data in Mongo DB to streamline the components which increases the efficiency and decreases the turnaround time by 15%. Also, containerized the application using Docker. Created an internal application with React, closures, asynchronous programming for Commvault’s employees which serves as a shared component repository which reduced the development time by 40% and helps maintaining the product consistency. Experience in Redux, Context API to manage the application state. Made responsive web pages with media queries, CSS Grid, Css animation, Material UI and Flexbox. Also, made many Figma prototypes for Commvault’s design system site
+                Developed decoupled and scalable web applications by building micro-frontends with React, Redux, and Single SPA, while also integrating reusable React
+components into the Design system using the Storybook UI framework, resulting in cleaner code and a 20% increase in team productivity.
+
+Prepared detailed Unit Test cases and performed unit and regression testing using React Testing Library to maintain code quality that led to decreased
+bugs in User Acceptance Testing by 30%.
+
+Delivered engaging applications with user-centered design, utilizing Figma prototypes, responsive components, and a tech stack including HTML5, CSS3,
+JavaScript, JQuery, Ajax, JSON, and React, resulting in a 35% increase in user engagement.
+
+Enhanced UI for Commvault's Product Configuration Team, driving a 40% efficiency improvement in tracking and establishing customer subscriptions for
+annual and subscription-based products.
+
+
+Engineered cloud file storage optimization prototype with serverless REST APIs using Node.js, Express, MongoDB, and AWS services (S3 and Lambda)
+for enhanced efficiency.
+
+Introduced efficient state management using Redux and Context API, streamlining code development time by 40%.
+
+Optimized micro-frontends performance by 30% (measured by Lighthouse Report) through strategies such as minimizing re-renders, gzip compression,
+and implementing cache-policies.
 
                 </p>
               </div>
@@ -131,8 +150,9 @@ const WorkPage = () => {
                   Teaching Assistant</h1>
                   <h1 className="text-1xl">Jan 2022 - May 2022</h1>
                 <p className="mt-5 opacity-70">
-                Moved traditional approach of grading assignments manually which required multiple hours to automated grading using GitHub Actions and moving to GitHub Classroom which brought efficiency by 80%
-Helped professor for the course Design Patterns for assignment grading, conducting labs and keeping students updated about upcoming course requirements of a class of approximately 30 students
+                Helped professor for course Web Development for assignment grading, conducting labs & keeping students updated about course requirements of a
+class of 180 students. Moved manual grading approach to automated using GitHub Actions bringing efficiency by 80%.
+Conducted labs to teach practical assignments of many full stack technologies such as JavaScript, JQuery, React, Angular, Webpack, SCSS, Node, Express, MERN stack, etc. Shown strong communication and verbal skills to assist 80 students in resolving technical issues involving Git, MySQL, MongoDB queries. Helped performing code reviews, troubleshooting, debugging, resolving errors to improve code quality
                 </p>
               </div>
               <div className="w-2/4 rounded-xl overflow-hidden m-2">
@@ -151,15 +171,29 @@ Helped professor for the course Design Patterns for assignment grading, conducti
                   Software Deveoper</h1>
                   <h1 className="text-1xl">Nov 2018 - May 2022</h1>
                 <p className="mt-5 opacity-70">
-                Led the off-shore Tech-connect team to design, develop user test cases in accordance with user stories for FedEx Reverse Logix application. Also, estimated and delivered over 200 User Stories in Jira (Agile Project Management tool)  
-Increased the efficiency of the production deliveries by implementing automation using Python-Selenium Web Driver
+                Built a 95% highly efficient dashboard using Angular concepts of Directives, Services, Promises, Templating, Scope etc. while introducing PWA support for the largest logistics company (FedEx) by consolidating data from different APIs in a single AWS RDS database, leveraging new backend packages,
+procedures, and functions to provide real-time status on all service orders in one central location.
 
-Reduced production costs and defects for the project by leading the QA activities in delivering weekly sprints
+Integrated the legacy system with payment gateway utilizing Angular and changed the existing API to display the shipping cost dynamically for every
+location, increasing the efficiency of order handling by 30%.
 
-Proactively suggested improvements to the management team to reduce redundant work. Built websites using React and NodeJS following REST standard and Worked with Databases using MongoDB and implemented CRUD operations  
-Engineered a cronjob that will automatically deploy the latest build to the Kubernetes cluster using Jenkins reducing the manual build and deploy time by about 60% 
+Built highly scalable and available REST APIs in Java, improving production efficiency by 25% and successfully configuring services to handle GraphQL
+requests/responses, facilitating rapid development and deployment of new features.
 
-Collaborated with Business teams and designed, implemented strategic use cases to deliver successful production deliveries  
+Credited for successfully architecting and developing a cutting-edge system utilizing Java EE components such as Spring MVC, Spring IOC, and Spring
+security modules. Contributed to the development of data models using Hibernate POJOs and configuring the Hibernate persistence layer, ensuring
+efficient data management.
+Implemented robust API security measures, including OAuth2 authentication, JWT token validation, and encryption protocols, resulting in enhanced
+data accuracy and utmost protection for customers sensitive information.
+Took lead in performance tuning, being the sole contributor in reducing response time from 5 minutes to 30 seconds using SQL query optimization and
+table partitioning. Extracted valuable information from log events in the ELK stack (Elasticsearch, Logstash & Kibana) to collect data for capacity
+planning and costs per client.
+Collaborated and implemented a robust CI/CD pipeline using Jenkins with DevOps team, enabling seamless deployment of minified Angular files to AWS
+EC2 servers, showcasing interest in DevOps practices, and increasing production efficiency by 20%.
+Led the QA team, developing automated test cases utilizing debugging, UFT, QTP, Selenium Web Driver, and Jira. Created a Requirement Traceability
+Matrix and generated comprehensive testing reports, contributing to a 100% increase in production success rate.
+Enhanced frontend to support threaded messages and attach files, photos and videos to a task using AWS S3 Bucket.
+Collaborated with Business teams and designed, implemented strategic use cases to deliver successful production deliveries 
 Won SPOT award by CEO ,Rakesh Khanna. Won 3 SPOT and one value award with many clients’s appreciations
                 </p>
               </div>
